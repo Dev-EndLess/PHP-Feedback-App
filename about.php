842086022dcf4e63fc537faf0444e3d003f7a78d
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2024-04-12 17:31:23 +0200

    about.php/feedback.php/index.php

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php include 'containers/header.php'; ?>
+  
+    <div class="w-64 flex flex-col mx-auto items-center justify-center mt-64 sm:mt-24">
+
+    <h2 class="text-2xl text-indigo-600 text-center font-semibold">About</h2>
+
+    <p class="text-center mt-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore impedit totam porro iure reiciendis autem possimus sapiente, optio, exercitationem ipsum assumenda mollitia, recusandae expedita culpa ratione voluptatem esse quos quam?
+    </p>
+    </div>
+
+<?php include 'containers/footer.php'; ?>
